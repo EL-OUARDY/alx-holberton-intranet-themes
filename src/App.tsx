bdeacf9b68b7952bb8b3f0f4b0979827bd4c9fec
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-09-07 23:02:30 +0100

    Setup ShadCN + TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/ui/theme-toggle";
+import { ThemeProvider } from "./contexts/theme-provider";
 
 function App() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div id="ext-popup">
+        <ModeToggle />
+        <h1 className="text-3xl text-purple-500">Hello, World!</h1>
+        <Button>Click Me!</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
