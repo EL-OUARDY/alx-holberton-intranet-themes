ac2c0174871c65f091da4e037a66529b0512da03
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-09-08 00:15:11 +0100

    Use Inter font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import "@fontsource/inter";
+
 import { Button } from "./components/ui/button";
 import { ModeToggle } from "./components/ui/theme-toggle";
 import { ThemeProvider } from "./contexts/theme-provider";
@@ -8,7 +10,13 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div id="ext-popup">
         <ModeToggle />
-        <h1 className="text-3xl text-purple-500">Hello, World!</h1>
+        <h1 className="text-2xl text-purple-500">Google</h1>
+        <h1 className="text-2xl text-purple-500">Microsoft</h1>
+        <h1 className="text-2xl text-purple-500">Facebook</h1>
+        <h1 className="text-2xl text-purple-500">Amazon</h1>
+        <h1 className="text-2xl text-purple-500">Vercel</h1>
+        <h1 className="text-2xl text-purple-500">Twitter</h1>
+        <h1 className="text-2xl text-purple-500">Apple</h1>
         <Button>Click Me!</Button>
       </div>
     </ThemeProvider>
