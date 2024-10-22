0366c5a8cfa8c91a14a4fcb7b0305eb330fe0fa9
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-10-22 10:07:02 +0100

    Add themes

--- src/models/Theme.d.ts
+++ src/models/Theme.d.ts
@@ -9,7 +9,35 @@ export interface ITheme {
     github: string;
   };
   colors: {
-    background: string;
+    "primary-background": string;
+    "secondary-background": string;
+    "muted-background": string;
+    "primary-foreground": "#fff";
+    "secondary-foreground": string;
+    "muted-foreground": string;
+    primary: string;
+    "primary-accent": string;
+    secondary: string;
+    info: string;
+    success: string;
+    destructive: string;
+    border: string;
+    "code-background": string;
+    "code-foreground": string;
+    "primary-button": string;
+    "primary-button-hover": string;
+    "success-button": string;
+    "success-button-hover": string;
+    "secondary-button": string;
+    "secondary-button-hover": string;
+    "destructive-button": string;
+    "destructive-button-hover": string;
+    "user-text-select-color": string;
+    "user-text-select-background": string;
+  };
+  settings: {
+    "logo-width": "60px";
+    "logo-height": "60px";
   };
 }
 
